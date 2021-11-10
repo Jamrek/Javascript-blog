1c960ca2633f8399026af98f59d459914f1b766e
Karol Jamrozy <karol24@onet.pl>
2021-11-10 12:27:11 +0100

    remove class form links

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 function titleClickHandler(event) {
     console.log('Link was clicked!');
-    console.log(event);
-    // /* remove class 'active' from all article links  */ usunięcie klasy active, ze wszystkich linków 
+    // console.log(event);
 
-    // /* add class 'active' to the clicked link */ dodanie klasy active do wszystkich linków
+    /* remove class 'active' from all article links  */
+
+    /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
 
@@ -14,8 +15,21 @@ function titleClickHandler(event) {
     /* find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article */
+    const activeArticles = document.querySelectorAll("article");
 
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+    //     ctiveLink to pojedynczy link, spośród wyszukanych linków z klasą active,
+    // .classList to "biblioteka", zawierająca informacje i funkcje dotyczące klas tego elementu,
+    // .remove to jedna z tych funkcji, służącej do usunięcia klasy,
+    // ( ) – te nawiasy, umieszczone bezpośrednio po nazwie funkcji, mówią nam, że wykonujemy funkcję,
+    // 'active' to klasa, którą chcemy usunąć
 
+    // Przydatne finkcje classList, których można użyć analogicznie do remove:
+    // add - dodawanie klasy
+    // toggle - przełączanie klasy, jeśli element nie ma klasy to ją doda, a jeżeli ma klase to ją usunie
+    // contains - sprawdzenie, czy element posiada daną klase
 }
 
 const links = document.querySelectorAll('.titles a');
