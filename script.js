6d96d42ff845423cffa56ea50977b58c74b27d46
Karol Jamrozy <karol24@onet.pl>
2021-11-16 10:23:59 +0100

    remove content of articles

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 
 // var hw = require("taskrunner");
 
+
 function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
     const activeArticles = document.querySelectorAll("article");
 
+
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
 
-    // for (let articleSelector of articleSelector) {
-    //     articleSelector.classList.remove('active');
+    // for (let clickedElement of clickedElement) {
+    //     clickedElement.classList.remove('active');
     // }
     clickedElement.classList.add("active");
     console.log('Link was clicked!');
@@ -22,23 +24,6 @@ function titleClickHandler(event) {
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
     targetArticle.classList.add("active");
-    console.log(targetArticle);
-
-
-
-    /* remove class 'active' from all article links  */
-
-    /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
-
-    /* get 'href' attribute from the clicked link */
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
-
-
     //     ctiveLink to pojedynczy link, spośród wyszukanych linków z klasą active,
     // .classList to "biblioteka", zawierająca informacje i funkcje dotyczące klas tego elementu,
     // .remove to jedna z tych funkcji, służącej do usunięcia klasy,
@@ -57,3 +42,27 @@ for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
 // var hw = require("taskrunner");
+
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+// const articleId = getAttribute('id');
+
+
+// let html = '';
+
+function generateTitleLinks() {
+    const titleList = document.querySelector(optArticleSelector);
+    titleList.classList.remove('titles');
+    titleList.innerHTML = '';
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    // const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    for (let article of articles) {
+        const articleId = document.querySelector(optArticleSelector);
+
+    }
+}
+generateTitleLinks();
